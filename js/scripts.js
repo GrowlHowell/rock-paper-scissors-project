93bbe259dab579a65ed86ec6d4a7a77476421c3e
Justin Howell <justinmhowell@protonmail.com>
2022-07-22 21:09:06 -0400

    Create player choice prompt

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,25 @@ let getComputerChoice = function() {
     let choice = Math.floor((Math.random() * 3)) + 1;
     switch(choice) {
         case 1:
-            return "Rock";
+            return "rock";
             break;
         case 2:
-            return "Paper";
+            return "paper";
             break;
         case 3:
-            return "Scissors";
+            return "scissors";
             break;
         default:
             return "Didn't work!";
     }
 }
+
+let getPlayerChoice = function() {
+    let choice = prompt("What is your choice?").toLowerCase();
+    return choice;
+}
+
+let playerChoice = getPlayerChoice();
+let computerChoice = getComputerChoice();
+console.log("The computer chose " + computerChoice);
+console.log("The player chose " + playerChoice);
